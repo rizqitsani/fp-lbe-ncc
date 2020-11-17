d43c5b9ee4da2d290e897961c79c20025349ae04
gsihaj5 <gsihaj5@gmail.com>
2020-11-17 18:03:28 +0700

    formulir pengisian pokemon (main infonya saja)

---
+++ pokemon_main_info_form.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href = "style.css">
+</head>
+<body class = "pokemon_main_information_form">
+<div>INSERT POKEMON</div>
+<form method="POST" action="process_input.php">
+    <div class="form-section">
+        <div class = "form-title">MAIN INFORMATION</div>
+        <input name="name" type="text" placeholder="NAME">
+        <input name="type" type="text" placeholder="TYPE">
+        <input name="species" type="text" placeholder="SPECIES">
+        <input name="height" type="number" placeholder="HEIGHT">
+        <input name="weight" type="number" placeholder="WEIGHT">
+        <input name="attack" type="text" placeholder="ATTACK">
+        <input name="health_point" type="number" placeholder="HP">
+        <input name="speed" type="number" placeholder="SPEED">
+        <input name="speed_attack" type="number" placeholder="ATTACK SPEED">
+        <input name="speed_defense" type="number" placeholder="DEFENSE SPEED">
+    </div>
+    <input type="submit" value="input pokemon">
+</form>
+
+</body>
+</html>
---
+++ process_input.php
@@ -0,0 +1,7 @@
+<?php
+
+//TODO: implements process input
+
+//testing input
+
+Echo $_POST['name'];
