707541ef39a1003cce246df6bc1918decc19704e
gsihaj5 <gsihaj5@gmail.com>
2020-11-17 15:57:16 +0700

    modify sql add foreign key

--- database/Pokemon.sql
+++ database/Pokemon.sql
@@ -1,141 +1,93 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.13-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             11.0.0.6015
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
-
--- Dumping structure for table pokemon.breeding
-CREATE TABLE IF NOT EXISTS `breeding`
+create table breeding
 (
-    `Egg_Groups` varchar(250) NOT NULL,
-    `Gender`     varchar(60)  NOT NULL,
-    `Egg_Cycles` varchar(55)  NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    Egg_Groups varchar(250) not null,
+    Gender     varchar(60)  not null,
+    Egg_Cycles varchar(55)  not null
+);
 
--- Dumping data for table pokemon.breeding: ~0 rows (approximately)
-/*!40000 ALTER TABLE `breeding`
-    DISABLE KEYS */;
-/*!40000 ALTER TABLE `breeding`
-    ENABLE KEYS */;
-
--- Dumping structure for table pokemon.egg_moves
-CREATE TABLE IF NOT EXISTS `egg_moves`
+create table moves
 (
-    `ID`    int(11)      NOT NULL,
-    `Move`  varchar(120) NOT NULL,
-    `Type`  varchar(60)  NOT NULL,
-    `Cat`   varchar(40)  NOT NULL,
-    `Power` int(11)      NOT NULL,
-    `Acc`   int(11)      NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    ID     int          not null
+        primary key,
+    Name   varchar(25)  not null,
+    Type   varchar(60)  not null,
+    Cat    varchar(30)  not null,
+    Power  int          not null,
+    Acc    int          not null,
+    Pp     int          not null,
+    Effect varchar(512) not null
+);
 
--- Dumping data for table pokemon.egg_moves: ~0 rows (approximately)
-/*!40000 ALTER TABLE `egg_moves`
-    DISABLE KEYS */;
-/*!40000 ALTER TABLE `egg_moves`
-    ENABLE KEYS */;
-
--- Dumping structure for table pokemon.moves
-CREATE TABLE IF NOT EXISTS `moves`
+create table pokemon
 (
-    `ID`     int(11)      NOT NULL,
-    `Name`   varchar(25)  NOT NULL,
-    `Type`   varchar(60)  NOT NULL,
-    `Cat`    varchar(30)  NOT NULL,
-    `Power`  int(11)      NOT NULL,
-    `Acc`    int(11)      NOT NULL,
-    `Pp`     int(11)      NOT NULL,
-    `Effect` varchar(512) NOT NULL,
-    PRIMARY KEY (`ID`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    Id            int auto_increment
+        primary key,
+    Nama          varchar(250) not null,
+    Type          varchar(55)  not null,
+    Species       varchar(60)  not null,
+    Height        varchar(75)  not null,
+    Weight        varchar(100) not null,
+    Chenges       varchar(60)  not null,
+    Attack        int          not null,
+    Health_Point  int          not null,
+    Speed         int          not null,
+    Speed_Attack  int          not null,
+    Speed_Defense int          not null
+);
 
--- Dumping data for table pokemon.moves: ~0 rows (approximately)
-/*!40000 ALTER TABLE `moves`
-    DISABLE KEYS */;
-/*!40000 ALTER TABLE `moves`
-    ENABLE KEYS */;
-
--- Dumping structure for table pokemon.pokemon
-CREATE TABLE IF NOT EXISTS `pokemon`
+create table abilities
 (
-    `Id`            int(11)      NOT NULL AUTO_INCREMENT,
-    `Nama`          varchar(250) NOT NULL,
-    `Type`          varchar(55)  NOT NULL,
-    `Species`       varchar(60)  NOT NULL,
-    `Height`        varchar(75)  NOT NULL,
-    `Weight`        varchar(100) NOT NULL,
-    `Chenges`       varchar(60)  NOT NULL,
-    `Attack`        int(11)      NOT NULL,
-    `Health_Point`  int(11)      NOT NULL,
-    `Speed`         int(11)      NOT NULL,
-    `Speed_Attack`  int(11)      NOT NULL,
-    `Speed_Defense` int(11)      NOT NULL,
-    PRIMARY KEY (`Id`),
-    KEY `Id` (`Id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    Id         int auto_increment
+        primary key,
+    pokemon_id int          not null,
+    Nama       varchar(250) not null,
+    effect     varchar(250) not null,
+    constraint abilities_ibfk_1
+        foreign key (pokemon_id) references pokemon (Id)
+);
 
-CREATE TABLE IF NOT EXISTS abilities
+create index pokemon_id
+    on abilities (pokemon_id);
+
+create table egg_moves
 (
-    Id         int(11)      NOT NULL AUTO_INCREMENT,
-    pokemon_id int(11)      NOT NULL,
-    Nama       varchar(250) NOT NULL,
-    effect     varchar(250) NOT NULL,
-    PRIMARY KEY (`Id`),
-    FOREIGN KEY (pokemon_id) REFERENCES pokemon (Id)
+    ID         int          not null,
+    pokemon_id int          not null,
+    Move       varchar(120) not null,
+    Type       varchar(60)  not null,
+    Cat        varchar(40)  not null,
+    Power      int          not null,
+    Acc        int          not null,
+    constraint egg_moves_ibfk_1
+        foreign key (pokemon_id) references pokemon (Id)
+);
 
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+create index pokemon_id
+    on egg_moves (pokemon_id);
 
--- Dumping data for table pokemon.pokemon: ~0 rows (approximately)
-/*!40000 ALTER TABLE `pokemon`
-    DISABLE KEYS */;
-/*!40000 ALTER TABLE `pokemon`
-    ENABLE KEYS */;
+create index Id
+    on pokemon (Id);
 
--- Dumping structure for table pokemon.training
-CREATE TABLE IF NOT EXISTS `training`
+create table training
 (
-    `Ev_Yield`        varchar(120) NOT NULL,
-    `Catch_Rate`      varchar(55)  NOT NULL,
-    `Base_Friendship` varchar(55)  NOT NULL,
-    `Base_Exp`        int(11)      NOT NULL,
-    `Growth_Rate`     varchar(55)  NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    Ev_Yield        varchar(120) not null,
+    Catch_Rate      varchar(55)  not null,
+    Base_Friendship varchar(55)  not null,
+    Base_Exp        int          not null,
+    Growth_Rate     varchar(55)  not null,
+    pokemon_id      int          not null,
+    constraint training_pokemon_Id_fk
+        foreign key (pokemon_id) references pokemon (Id)
+);
 
--- Dumping data for table pokemon.training: ~0 rows (approximately)
-/*!40000 ALTER TABLE `training`
-    DISABLE KEYS */;
-/*!40000 ALTER TABLE `training`
-    ENABLE KEYS */;
-
--- Dumping structure for table pokemon.type_defense
-CREATE TABLE IF NOT EXISTS `type_defense`
+create table type_defense
 (
-    `ID`    int(11)     NOT NULL AUTO_INCREMENT,
-    `Power` varchar(60) NOT NULL,
-    `Point` float       NOT NULL,
-    PRIMARY KEY (`ID`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    ID         int auto_increment
+        primary key,
+    Power      varchar(60) not null,
+    Point      float       not null,
+    pokemon_id int         not null,
+    constraint type_defense_pokemon_Id_fk
+        foreign key (pokemon_id) references pokemon (Id)
+);
 
--- Dumping data for table pokemon.type_defense: ~0 rows (approximately)
-/*!40000 ALTER TABLE `type_defense`
-    DISABLE KEYS */;
-/*!40000 ALTER TABLE `type_defense`
-    ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE = IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS = IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
